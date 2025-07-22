56ff702dc4f38c2cf1cea7aa315a253d8a4e0b6e
Aakamsh PM <aakamshpm@gmail.com>
2025-07-22 18:34:06 +0530

    tasks: controller and module

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { CurrentUser } from 'src/common/decorators/user.decorator';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Controller()
+@UseGuards(JwtAuthGuard)
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  create(@Body() createTaskDto: CreateTaskDto, @CurrentUser() user: any) {
+    return this.tasksService.create(createTaskDto, user.id);
+  }
+
+  @Get()
+  findAll(@CurrentUser() user: any) {
+    return this.tasksService.findAll(user.id);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string, @CurrentUser() user: any) {
+    return this.tasksService.findOne(id, user.id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+    @CurrentUser() user: any,
+  ) {
+    return this.tasksService.update(id, updateTaskDto, user.id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string, @CurrentUser() user: any) {
+    return this.tasksService.remove(id, user.id);
+  }
+}
